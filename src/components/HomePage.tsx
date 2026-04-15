f5e7a150ea549ab671f2ada689b06301f86b811e
dlt <2784680477@qq.com>
2026-04-15 21:35:10 +0800

    样式修改

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -21,23 +21,23 @@ const HomePage: React.FC = () => {
         <div className="feature-cards">
           <div className="feature-card">
             <FontAwesomeIcon icon={faBook} size="2x" className="feature-icon" />
-            <h3>错题集</h3>
-            <p>记录错题</p>
+            <h3>任务管理</h3>
+            <p>创建和管理每日学习任务，跟踪学习进度</p>
           </div>
           <div className="feature-card">
             <FontAwesomeIcon icon={faClock} size="2x" className="feature-icon" />
-            <h3>发发牢骚</h3>
-            <p>跟踪学习进度，提高学习效率</p>
+            <h3>时间统计</h3>
+            <p>记录学习时长，提高学习效率</p>
           </div>
           <div className="feature-card">
             <FontAwesomeIcon icon={faCalendarCheck} size="2x" className="feature-icon" />
-            <h3>打卡记录</h3>
+            <h3>每日打卡</h3>
             <p>养成良好学习习惯，坚持每日打卡</p>
           </div>
           <div className="feature-card">
             <FontAwesomeIcon icon={faBookOpen} size="2x" className="feature-icon" />
-            <h3>择校信息</h3>
-            <p>记录学习心得，总结学习经验</p>
+            <h3>学习计划</h3>
+            <p>制定科学的学习计划，合理分配时间</p>
           </div>
         </div>
       </div>
