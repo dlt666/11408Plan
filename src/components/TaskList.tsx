cf593f111c6249bec6d9c591558e0a0559ef1e05
dlt <2784680477@qq.com>
2026-04-17 17:01:58 +0800

    修不好了回退

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -460,7 +460,7 @@ const TaskList: React.FC = () => {
       <h2>每日学习任务</h2>
       {tasks && tasks.length > 0 ? (
         tasks.map(task => (
-          <div key={task.id} className="task-container">
+          <React.Fragment key={task.id}>
             <div 
               className="task-item" 
               onClick={() => navigate(`/tasks/${task.id}`)}
@@ -647,7 +647,7 @@ const TaskList: React.FC = () => {
                 </div>
               </div>
             )}
-          </div>
+          </React.Fragment>
         ))
       ) : (
         <div className="no-tasks">
